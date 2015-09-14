d82cfeccc0dec0413af634bad23bbe484affe2ac
John Stovin <john.d.stovin@gmail.com>
2015-09-14 22:22:12 +0100

    Height History

--- WeightTracker/WeightTracker.Shared/AppModel/Person.cs
+++ WeightTracker/WeightTracker.Shared/AppModel/Person.cs
@@ -14,14 +14,20 @@ namespace WeightTracker.AppModel
                 { new DateTime(2015, 9, 7), 95.8f },
                 { new DateTime(2015, 9, 8), 95.8f },
                 { new DateTime(2015, 9, 9), 96.0f },
-                {  new DateTime(2015, 9, 10), 96.6f },
-                {  new DateTime(2015, 9, 11), 97.0f },
+                { new DateTime(2015, 9, 10), 96.6f },
+                { new DateTime(2015, 9, 11), 97.0f },
                 { new DateTime(2015, 9, 12), 96.6f },
                 { new DateTime(2015, 9, 13), 96.8f },
+                { new DateTime(2015, 9, 14), 96.2f },
+            };
+
+            HeightHistory = new Dictionary<DateTime, float>
+            {
+                { new DateTime(2015, 9, 4), 1.76f },
             };
         }
         
-        public float Height { get; private set; } = (float) 1.76;
+        public IDictionary<DateTime, float> HeightHistory { get; private set; }
 
         public IDictionary<DateTime, float> WeightHistory { get; private set; }
     }
